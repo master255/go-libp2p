be9595128ed7ba5d6dd9f4af75a267d84d19ca43
Master255 <masters@inbox.ru>
2023-06-10 04:41:27 +0300

    Adding UserAgent to Nat for description of port mapping

--- p2p/host/basic/options.go
+++ p2p/host/basic/options.go
@@ -5,7 +5,9 @@ type Option func(*natManager) error
 // WithUserAgent is a natManager option that sets specific user agent for the NAT manager.
 func WithUserAgent(userAgent string) Option {
 	return func(nmgr *natManager) error {
-		nmgr.userAgent = userAgent
+		if userAgent != "" {
+			nmgr.userAgent = userAgent
+		}
 		return nil
 	}
 }
